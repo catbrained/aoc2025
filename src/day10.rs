8684aa2d712e510a5273580853d50c144a5bdf18
Linda Siemons <linda@catbrained.dev>
2025-12-13 00:53:47 +0100

    WIP: gaussian elimination
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- src/day10.rs
+++ src/day10.rs
@@ -6,11 +6,18 @@ pub fn solve_puzzle_a(input: &str) -> usize {
     machines.map(|m: Machine| m.configure()).sum()
 }
 
+pub fn solve_puzzle_b(input: &str) -> usize {
+    let machines = input.lines().map(|l| l.into());
+
+    machines.map(|m: Machine| m.configure_joltage()).sum()
+}
+
 type Button = HashSet<usize>;
 
 #[derive(Debug)]
 struct Machine {
     target: HashSet<usize>,
+    joltage_target: Vec<usize>,
     buttons: Vec<Button>,
 }
 
@@ -42,6 +49,73 @@ impl Machine {
 
         unreachable!("Machine should be configurable");
     }
+
+    fn configure_joltage(&self) -> usize {
+        let rows = self.joltage_target.len();
+        let cols = self.buttons.len() + 1;
+        let mut matrix = Vec::with_capacity(rows * cols);
+        for (target_idx, &target) in self.joltage_target.iter().enumerate() {
+            for button in self.buttons.iter() {
+                let b = if button.contains(&target_idx) {
+                    1.0
+                } else {
+                    0.0
+                };
+                matrix.push(b);
+            }
+            matrix.push(target as f64);
+        }
+        debug_assert_eq!(matrix.len(), rows * cols);
+        for row in 0..rows {
+            println!("{:+.2?}", &matrix[(row * cols)..(row * cols + cols)]);
+        }
+        println!("_______________");
+
+        // See: `https://en.wikipedia.org/wiki/Gaussian_elimination`
+        const EPSILON: f64 = 1.0E-10;
+        let mut pivot_row = 0;
+        let mut pivot_col = 0;
+        while pivot_row < rows && pivot_col < cols {
+            // Find pivot
+            let mut max = f64::MIN;
+            let mut max_idx = pivot_row;
+            for row in pivot_row..rows {
+                let c = matrix[row * cols + pivot_col].abs();
+                if c > max {
+                    max = c;
+                    max_idx = row;
+                }
+            }
+            // Check if found pivot is zero.
+            // (With tolerance to account for floating point precision limits)
+            if matrix[max_idx * cols + pivot_col].abs() <= EPSILON {
+                // No pivot in this column. Move to next one.
+                pivot_col += 1;
+            } else {
+                // Swap rows
+                for col in 0..cols {
+                    matrix.swap(pivot_row * cols + col, max_idx * cols + col);
+                }
+                // For all rows below pivot
+                for row in (pivot_row + 1)..rows {
+                    let factor =
+                        matrix[row * cols + pivot_col] / matrix[pivot_row * cols + pivot_col];
+                    // Set lower part of pivot column to zero.
+                    matrix[row * cols + pivot_col] = 0.0;
+                    // Subtract multiplied pivot row from remaining elements of current row.
+                    for col in (pivot_col + 1)..cols {
+                        matrix[row * cols + col] -= matrix[pivot_row * cols + col] * factor;
+                    }
+                }
+                pivot_col += 1;
+                pivot_row += 1;
+            }
+        }
+        for row in 0..rows {
+            println!("{:+.2?}", &matrix[(row * cols)..(row * cols + cols)]);
+        }
+        todo!()
+    }
 }
 
 #[derive(Debug)]
@@ -53,6 +127,7 @@ struct Node {
 impl From<&str> for Machine {
     fn from(value: &str) -> Self {
         let mut target = HashSet::new();
+        let mut joltage_target = Vec::new();
         let mut buttons = Vec::new();
         let elements = value.split_whitespace();
         for elem in elements {
@@ -76,18 +151,27 @@ impl From<&str> for Machine {
                     }
                     buttons.push(button);
                 }
-                b'{' => continue,
+                b'{' => {
+                    let nums = elem[1..elem.len() - 1].split(',');
+                    for n in nums.map(|n| n.parse::<usize>().unwrap()) {
+                        joltage_target.push(n);
+                    }
+                }
                 _ => panic!("unknown element"),
             }
         }
 
-        Self { target, buttons }
+        Self {
+            target,
+            joltage_target,
+            buttons,
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Machine, solve_puzzle_a};
+    use super::{Machine, solve_puzzle_a, solve_puzzle_b};
 
     #[test]
     fn example() {
@@ -104,5 +188,22 @@ mod tests {
         assert_eq!(machines.next().unwrap().configure(), 2);
         assert_eq!(machines.next().unwrap().configure(), 3);
         assert_eq!(machines.next().unwrap().configure(), 2);
+    }
+
+    #[test]
+    fn example2() {
+        let input = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\n[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\n[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}\n";
+
+        assert_eq!(solve_puzzle_b(input), 33);
+    }
+
+    #[test]
+    fn example_machines2() {
+        let input = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\n[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\n[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}\n";
+        let mut machines = input.lines().map(Machine::from);
+
+        assert_eq!(machines.next().unwrap().configure_joltage(), 10);
+        assert_eq!(machines.next().unwrap().configure_joltage(), 12);
+        assert_eq!(machines.next().unwrap().configure_joltage(), 11);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -130,4 +130,10 @@ fn main() {
     let input = read_to_string("./day_10_a_input.txt").unwrap();
     let result = day10::solve_puzzle_a(&input);
     writeln!(output, "Solution: `{result}`").unwrap();
+
+    // Day 10 - Second puzzle
+    writeln!(output, "=== Day 10 - second puzzle ===").unwrap();
+    // Uses the same input as the previous puzzle
+    let result = day10::solve_puzzle_b(&input);
+    writeln!(output, "Solution: `{result}`").unwrap();
 }
